47cbf0dfceb474cf87a7d6ce3a7ad34664032db8
Shotaro Hirose <shotaro199959@icloud.com>
2023-02-16 00:10:06 +0900

    iOSのスプラッシュスクリーン対応

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,8 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#000000" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <script async src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.9/pwacompat.min.js" integrity="sha384-VcI6S+HIsE80FVM1jgbd6WDFhzKYA0PecD/LcIyMQpT4fMJdijBh0I7Iblaacawc" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
